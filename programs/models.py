2a46c61fe859bf183d6849dd28d592d0a79c3be9
kweku-xvi <nksarps@gmail.com>
2024-03-12 14:25:33 +0000

    added event model

--- programs/models.py
+++ programs/models.py
@@ -1,3 +1,33 @@
+import uuid
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    id = models.CharField(max_length=15, primary_key=True, unique=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    poster = models.ImageField(blank=True, null=True)
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField(blank=True, null=True)
+    short_description = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    maps_link = models.CharField(max_length=255, blank=True, null=True)
+    event_link = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = str(uuid.uuid4())[:15]
+        super().save(*args, **kwargs)
+
+
+    class Meta:
+        ordering = ('-created_at',)
