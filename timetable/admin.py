6606097f0769474373f5c3e564be013bb44234a2
kweku-xvi <nksarps@gmail.com>
2024-03-19 18:17:20 +0000

    added courses, lecturers and classes to admin panel

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,3 +1,19 @@
+from .models import Course, Lecturer, Class
 from django.contrib import admin
 
-# Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_code', 'course_name', 'course_type', 'credit_hours')
+
+
+class LecturerAdmin(admin.ModelAdmin):
+    list_display = ('lecturer_id', 'name', 'email', 'office_location')
+
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('class_id', 'course', 'lecturer', 'week_day', 'time_start', 'time_end')
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lecturer, LecturerAdmin)
+admin.site.register(Class, ClassAdmin)
